8fc3b96d982e87e445af2ef6a7562ce960b06e9d
gabrielabrag <gabi.rod.braga@hotmail.com>
2022-07-22 11:03:44 -0300

    Initial commit

---
+++ cadastro.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+#layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key= 'usuario',size=(20,1))],
+    [sg.Text('Senha') , sg.Input(key='senha' , password_char= '*',size=(20,1) )],
+    [sg.Checkbox('Salvar login?')],
+    [sg.Button('Entrar')]
+]
+#janela
+janela = sg.Window('Tela de login',layout)
+#ler eventos
+while True:
+   eventos, valores = janela.read()
+   if eventos == sg.WINDOW_CLOSED:
+       break
+   if eventos == 'Entrar':
+       if valores['usuario'] == 'Gabriela' and valores['senha'] == '123456':
+           print('Bem Vinda')
